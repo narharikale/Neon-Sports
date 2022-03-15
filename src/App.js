25500e829bf3b9aa17788ae1e1aeca5456b19ff5
narharikale <naraharikale5051@gmail.com>
2022-03-15 18:01:03 +0530

    added pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
 import Mockman from 'mockman-js'
+import Navbar from './components/navbar/navbar.jsx'
+import { Route, Routes } from "react-router-dom";
+import { Home ,ProductListing, Cart, ForgotPassword, Login, SignUp, Wishlist } from "./pages/export";
 
 function App() {
   return (
     <div className="App">
-      <Mockman/>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/productlisting" element={<ProductListing/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/wishlist" element={<Wishlist/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<SignUp/>} />
+        <Route path="/forgotpassword" element={<ForgotPassword/>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/export.js
@@ -0,0 +1,7 @@
+export { Home } from './home/home';
+export { Cart } from './cart/cart';
+export { ProductListing } from './productListing/productListing';
+export { Wishlist } from './wishlist/wishlist';
+export { Login } from './auth/login';
+export { SignUp } from './auth/signup';
+export { ForgotPassword } from './auth/forgot-password';
