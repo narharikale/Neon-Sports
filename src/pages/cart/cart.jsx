d47767a426713a6faca6722e4d2ef4bb6bfa7506
narharikale <naraharikale5051@gmail.com>
2022-03-26 12:35:17 +0530

    feat : added card and price detail component

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,8 +1,23 @@
+import { CartProductCard, PriceDetailsCard } from "../../components";
+
+
+
+
+
+
+
 function Cart() {
+
+
     return (  
-        <>
         
-        </>
+             <div className="cart-container"> 
+                <div className="d-flex flex-column gap-2"> 
+                    <CartProductCard/>
+                </div>
+                <PriceDetailsCard/>
+             </div>
+        
     );
 }
 
