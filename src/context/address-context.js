83e3604c558534665145f9483605bb4c17213c15
narharikale <naraharikale5051@gmail.com>
2022-06-20 12:35:14 +0530

    `fix` : auth error

--- src/context/address-context.js
+++ src/context/address-context.js
@@ -1,88 +1,91 @@
-import { createContext, useContext, useState , useEffect } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import axios from "axios";
+import { useAuth } from "./auth-context";
 
 const addressContext = createContext(null);
 const useAddress = () => useContext(addressContext);
 
 
 function AddressProvider({ children }) {
-    const token = localStorage.getItem('AuthToken')
-    const [addresses , setAddresses]  = useState([])
+  const token = localStorage.getItem('AuthToken');
+  const [addresses, setAddresses] = useState([])
+  const { isAuth } = useAuth()
 
-    
-    useEffect(() => {
-        (async function getAddress() {
-          try {
-            const { data } = await axios.get("/api/user/address", {
-              headers: {
-                authorization: token,
-              },
-            });
-            setAddresses(data.address)
-
-          } catch (err) {
-            console.error(err);
-          }
-        })();
-      }, []);
-    
-      const addAddress = async(address) => {
+  useEffect(() => {
+    if (isAuth) {
+      (async function getAddress() {
         try {
-          const { data } = await axios.post(
-            "/api/user/address",
-            {
-              address,
+          const { data } = await axios.get("/api/user/address", {
+            headers: {
+              authorization: token,
             },
-            {
-              headers: {
-                authorization: token,
-              },
-            }
-            );
-          setAddresses(data.address);
+          });
+          setAddresses(data.address)
+
         } catch (err) {
-          console.error(err);
+          console.error(err, 'err in getaddress');
         }
-      };
+      })();
+    }
+  }, [isAuth]);
 
-      const updateAddress = async (address) => {
-        try {
-            const { data } = await axios.post(
-                `/api/user/address/${address._id}`,
-                {
-                  address,
-                },
-                {
-                    headers: { authorization: token },
-                });
-            setAddresses(data.address)
-        }
-        catch(err){
-            console.error(err);
-        }     
-    };
-
-    const removeAddress = async (addressId) => {
-        try {
-        const { data } = await axios.delete(`/api/user/address/${addressId}`, {
+  const addAddress = async (address) => {
+    try {
+      const { data } = await axios.post(
+        "/api/user/address",
+        {
+          address,
+        },
+        {
           headers: {
             authorization: token,
           },
+        }
+      );
+      setAddresses(data.address);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const updateAddress = async (address) => {
+    try {
+      const { data } = await axios.post(
+        `/api/user/address/${address._id}`,
+        {
+          address,
+        },
+        {
+          headers: { authorization: token },
         });
-        setAddresses(data.address);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-      
+      setAddresses(data.address)
+    }
+    catch (err) {
+      console.error(err);
+    }
+  };
 
-    return (
+  const removeAddress = async (addressId) => {
+    try {
+      const { data } = await axios.delete(`/api/user/address/${addressId}`, {
+        headers: {
+          authorization: token,
+        },
+      });
+      setAddresses(data.address);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-        <addressContext.Provider value={{ addAddress , addresses , updateAddress , removeAddress }} >
-            {children}
-        </addressContext.Provider>
 
-    );
+  return (
+
+    <addressContext.Provider value={{ addAddress, addresses, updateAddress, removeAddress }} >
+      {children}
+    </addressContext.Provider>
+
+  );
 }
 
 export { AddressProvider, useAddress };
